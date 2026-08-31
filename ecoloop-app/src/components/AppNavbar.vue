7aa80098e7b88808b541700fcdd35ca04bd85063
CallMeNephilim <mahdiafkar0@gmail.com>
2026-08-31 17:59:42 +1000

    Navigation Bar Component vue file

---
+++ ecoloop-app/src/components/AppNavbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success shadow-sm mb-4">
+    <div class="container-fluid container-xxl">
+      <a class="navbar-brand fw-bold" href="#">
+        🌱 Eco Loop Melbourne
+      </a>
+      <span class="navbar-text text-light fst-italic d-none d-sm-inline">
+        Waste Reduction & Circular Economy Hub
+      </span>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+// navigation component 4 my Eco Loop Melbourne
+</script>
